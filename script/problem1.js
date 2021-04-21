78f05855cc0c5ae70956587759b326def03c54f4
kylenguyen1008 <marinbnl001@gmail.com>
2021-04-21 11:17:12 +0700

    update problem 1

--- script/problem1.js
+++ script/problem1.js
@@ -1,15 +1,35 @@
-let isUnderstand = false;
+// let isUnderstand = false;
 
-if (isUnderstand == false) {
+// if (isUnderstand == false) {
 
-document.getElementById('understand').onclick = () => {
+// document.getElementById('understand').onclick = () => {
+//     document.getElementById('overlay-cookies').style.display = 'none';
+//     document.getElementById('box-message').style.display = 'none';
+//     document.getElementById('full-html').style.overflow = 'scroll';
+//     document.getElementById('full-html').style.overflowX = 'hidden';
+//     isUnderstand = true;
+//     console.log(isUnderstand)
+
+// }
+
+// }
+
+const cookieContainer = document.querySelector('.cookie-container');
+const cookieButton = document.querySelector('.cookie-btn');
+
+cookieButton.addEventListener("click", () => {
+    cookieContainer.classList.remove("active");
+    localStorage.setItem("cookieBannerDisplayed", "true");
     document.getElementById('overlay-cookies').style.display = 'none';
-    document.getElementById('box-message').style.display = 'none';
     document.getElementById('full-html').style.overflow = 'scroll';
     document.getElementById('full-html').style.overflowX = 'hidden';
-    isUnderstand = true;
-    console.log(isUnderstand)
+});
 
-}
+setTimeout( () => {
+    if (!localStorage.getItem("cookieBannerDisplayed")) {
+        cookieContainer.classList.add("active");
+        document.getElementById('overlay-cookies').style.display = 'block';
+    }
+}, 1000);
 
-}
+
