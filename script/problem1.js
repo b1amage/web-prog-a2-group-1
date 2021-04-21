e5a4ad24853e6f5277a81ea340e731ae21895144
kylenguyen1008 <marinbnl001@gmail.com>
2021-04-21 15:43:44 +0700

    add comment and clean code js problem 1

--- script/problem1.js
+++ script/problem1.js
@@ -1,33 +1,33 @@
-// let isUnderstand = false;
-
-// if (isUnderstand == false) {
-
-// document.getElementById('understand').onclick = () => {
-//     document.getElementById('overlay-cookies').style.display = 'none';
-//     document.getElementById('box-message').style.display = 'none';
-//     document.getElementById('full-html').style.overflow = 'scroll';
-//     document.getElementById('full-html').style.overflowX = 'hidden';
-//     isUnderstand = true;
-//     console.log(isUnderstand)
-
-// }
-
-// }
-
+// Create consts from the html file by using querySelector method for shoter usage later
 const cookieContainer = document.querySelector('.cookie-container');
 const cookieButton = document.querySelector('.cookie-btn');
 
+// Create event when the button "I understand" is clicked
 cookieButton.addEventListener("click", () => {
+
+    // Remove the CSS avtive <=> remove the bottom 40% attribute of the block to make the message pop up on the screen
     cookieContainer.classList.remove("active");
+
+    // Create a variable in the local storage to checked if it is clicked
     localStorage.setItem("cookieBannerDisplayed", "true");
+
+    // If the button is clicked, we will not display the box anymore, set the display atttribute to none
     document.getElementById('overlay-cookies').style.display = 'none';
+
+    // Unfrozen the html file for the user to scroll after clicking the button
     document.getElementById('full-html').style.overflow = 'scroll';
     document.getElementById('full-html').style.overflowX = 'hidden';
 });
 
+// Set time for the smooth animation in after 1 second
 setTimeout( () => {
+    
+    // If the variable cookieBannerDisplayed is not exist, we start the code block
     if (!localStorage.getItem("cookieBannerDisplayed")) {
+        // Change the active CSS for the box message to pop up 
         cookieContainer.classList.add("active");
+
+        // Display the overlay to avoid the user to click or hover to the main website
         document.getElementById('overlay-cookies').style.display = 'block';
     }
 }, 1000);
