737c9e1e9452d4517c9e62cade6be72b62ccfeae
s3878694 <s3878694@rmit.edu.vn>
2021-05-05 22:43:46 +0700

    valid checkbox not working

--- script/problem4.js
+++ script/problem4.js
@@ -165,16 +165,44 @@ function validMessage(){
     }
 }
 
+// Function to have at leaste one radio is checked
+function validRadio(){
+    var radio_alert = document.getElementById("radio_alert");
+    if (document.getElementById("email_method").checked == false && document.getElementById("phone_method").checked == false){
+        radio_alert.classList.add("a");
+        radio_alert.classList.remove("b");
+        radio_alert.innerHTML = "You have to choose one option";
+        return false;
+    } else {
+        radio_alert.classList.add("b");
+        radio_alert.classList.remove("a");
+        radio_alert.innerHTML = "Valid";
+        return true;
+    }
+}
+
+// Function that have at lease checkbox is checked
+function validCheckbox(){ 
+    var checkbox_alert =  document.getElementsById("checkbox_alert");
+    if ( document.getElementById("monday").checked == false && document.getElementById("tuesday").checked == false && document.getElementById("wednesday").checked == false && document.getElementById("thursday").checked == false && document.getElementById("friday").checked == false && document.getElementById("saturday").checked == false && document.getElementById("sunday").checked== false ){
+        checkbox_alert.classList.add("a");
+        checkbox_alert.classList.remove("b");
+        checkbox_alert.innerHTML = "You need to choose one date";
+        return false;
+    } else {
+        return true;
+    }
+}
+
 // Inform the users whether the inputs are valid or not while they are typing
 document.getElementById("full_name").addEventListener("input", validateName);
 document.getElementById("email").addEventListener("input", validateEmail);
 document.getElementById("phone").addEventListener("input", validatePhone);
 
 
-
 //Valdiate all fields
 function validateform(event){
-    if(!validateName() || !validateEmail() || !validatePhone() || !validMessage()) {
+    if(!validateName() || !validateEmail() || !validatePhone() || !validRadio() || !validCheckbox() || !validMessage()) {
         event.preventDefault(); // If there is invalid input, prevent the form from being submitted by cancelling the event
     } else {
         return true; // Submit the form when all inputs are valid
@@ -182,4 +210,4 @@ function validateform(event){
 }
 
 // Submit all form
-document.querySelector("form").addEventListener("submit", validateform)
+document.querySelector("form").addEventListener("submit", validateform);
