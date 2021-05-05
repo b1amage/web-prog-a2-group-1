b16c2f194a35936f676b17a4ee8bc8353955bdc3
TuanDao2002 <s3877347@rmit.edu.vn>
2021-05-05 23:18:37 +0700

    update

--- script/automatic_scroll.js
+++ script/automatic_scroll.js
@@ -121,9 +121,5 @@ function autoScroll(containerName, item) {
 
 autoScroll("store-container", "store");
 autoScroll("product-container", "product");
-<<<<<<< HEAD
 autoScroll("feature-container", "feature");
-=======
-autoScroll("feature-container", "feature");
-autoScroll("feature-container last", "feature");
->>>>>>> 97bcc3935128e7bd8f8501f21fe740a923a3c604
+// autoScroll("feature-container last", "feature");
