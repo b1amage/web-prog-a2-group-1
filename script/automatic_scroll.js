ec39dd02895dbf45a2be6b1718e53bddcbeb48ec
ajking02008 <duynguyen02008@gmail.com>
2021-05-07 09:36:08 +0700

    update scroll

--- script/automatic_scroll.js
+++ script/automatic_scroll.js
@@ -1,61 +1,3 @@
-// // Initialising parameters for scrolling events
-// var stop = 'false';
-// var duplicate = 'true';
-// var container = document.getElementById("scroll");
-// var stores = document.getElementsByClassName("store");
-// var horMargin = 50;
-
-// // Duplicate the length of the container
-// function setup() {
-//     let intialLength = stores.length;
-//     for (let i = 0; i < intialLength; i++) {
-//         container.appendChild(stores[i].cloneNode(true));
-//     }
-// }
-
-// // Create a scroll effect
-// function startScroll() {
-//     if (duplicate === 'true') {
-//         setup();
-//         duplicate = 'false';
-//     }
-
-//     let width = stores[0].offsetWidth;
-
-//     if (stop === 'false') {
-//         container.scrollLeft += 4;
-//     } 
-
-//     if (container.scrollLeft === 0) {
-//         let clone = stores[stores.length-1].cloneNode(true);
-//         container.removeChild(stores[stores.length-1]);
-//         container.prepend(clone);
-//         container.scrollLeft = width + horMargin * 2;
-//     }
-
-//     if (container.scrollLeft >= width + horMargin * 2) {
-//         let clone = stores[0].cloneNode(true);
-//         container.removeChild(stores[0]);
-//         container.appendChild(clone);
-//         container.scrollLeft = 0;
-//     }
-// }
-
-// function stopScroll() {
-//     stop = 'true';
-// }
-
-// function restartScroll() {
-//     stop = 'false';
-// }
-
-// if (stores.length > 5) {
-//     setInterval('startScroll()', 25);
-// }
-
-// container.onmouseover = stopScroll;
-// container.onmouseleave = restartScroll;
-
 var horMargin = 50;
 
 // Duplicate the length of the container
@@ -109,17 +51,16 @@ function autoScroll(containerName, item) {
     container.onmouseleave = function() {
         stop = 'false';
     }
-    setup(container, items);
     if (items.length > 5) {
+        setup(container, items);
         setInterval(function() {
             startScroll(container, items, stop);
         }, 25);
     }
 
-    
 }
 
 autoScroll("store-container", "store");
 autoScroll("product-container", "product");
 autoScroll("feature-container", "feature");
-autoScroll("feature-container last", "feature");
+autoScroll("feature-container last", "feature-last");
