520501516ed8f9c0a30a6e292216422d4e809f85
ajking02008 <duynguyen02008@gmail.com>
2021-05-08 16:37:07 +0700

    update about

--- script/display-member-info.js
+++ script/display-member-info.js
@@ -1,3 +1,4 @@
+// Initialise the variables
 var overlay = document.getElementById('overlay-about');
 var duyBox = document.getElementById('duy-box');
 var baoBox = document.getElementById('bao-box');
@@ -8,6 +9,7 @@ var duy = document.getElementById('more-info-duy');
 var long = document.getElementById('more-info-long');
 var tuan = document.getElementById('more-info-tuan');
 
+// Turn off overlay
 function offOverlay() {
     overlay.style.display = 'none';
     duy.style.display = 'none';
@@ -17,38 +19,27 @@ function offOverlay() {
     document.querySelector('html').style.overflowY = 'scroll';
 }   
 
+// Create an overlay when clicking on the box 
 duyBox.onclick = () => {
     document.querySelector('html').style.overflow = 'hidden';
     overlay.style.display = 'block';
     duy.style.display = 'block';
-    // duy.querySelector('h1').innerHTML = 'content here';
-    // duy.querySelector('p').innerHTML = 'content here';
-    // duy.querySelector('a').innerHTML = 'facebook link here';
 };
 
 baoBox.onclick = () => {
     document.querySelector('html').style.overflow = 'hidden';
     overlay.style.display = 'block';
     bao.style.display = 'block';
-    // bao.querySelector('h1').innerHTML = 'content here';
-    // bao.querySelector('p').innerHTML = 'content here';
-    // bao.querySelector('a').innerHTML = 'facebook link here';
 };
 
 longBox.onclick = () => {
     document.querySelector('html').style.overflow = 'hidden';
     overlay.style.display = 'block';
     long.style.display = 'block';
-    // long.querySelector('h1').innerHTML = 'content here';
-    // long.querySelector('p').innerHTML = 'content here';
-    // long.querySelector('a').innerHTML = 'facebook link here';
 };
 
 tuanBox.onclick = () => {
     document.querySelector('html').style.overflow = 'hidden';
     overlay.style.display = 'block';
     tuan.style.display = 'block';
-    // tuan.querySelector('h1').innerHTML = 'content here';
-    // tuan.querySelector('p').innerHTML = 'content here';
-    // tuan.querySelector('a').innerHTML = 'facebook link here';
 };
